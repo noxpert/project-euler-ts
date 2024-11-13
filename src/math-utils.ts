28f2c82a7e31930e1b79f970aa588f657428ca51
Matthew Morgenegg <noxpert@gmail.com>
2024-11-13 08:34:35 -0700

    Convert isDivisible to arrow function

--- src/math-utils.ts
+++ src/math-utils.ts
@@ -1,6 +1,4 @@
-export function isDivisible(value: number, factor: number): boolean {
-    return value % factor === 0;
-}
+export const isDivisible = (value: number, factor: number): boolean => value % factor === 0;
 
 export function isPrime(value: number): boolean {
     for (let num = 2; num < value / 2; num++) {
