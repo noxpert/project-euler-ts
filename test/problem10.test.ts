e69dd668613bd24636974f77f4af8ac9bb5db1a4
Matthew Morgenegg <noxpert@gmail.com>
2024-11-16 11:18:58 -0700

    Problem 10 test changes
    to improve test suite performance

--- test/problem10.test.ts
+++ test/problem10.test.ts
@@ -6,9 +6,15 @@ describe("Unit tests for problem10", () => {
         expect(answer).toBe(17);
     })
 
-    it("Test solution", () => {
-        let answer:number = sumOfPrimes(2_000_000);
-        expect(answer).toBe(142913828922);
+    // Commenting out so test suite runs faster
+    // it("Test solution", () => {
+    //     let answer:number = sumOfPrimes(2_000_000);
+    //     expect(answer).toBe(142913828922);
+    // })
+
+    it("Test larger number that doesn't take as long as 2 mil", () => {
+        let answer:number = sumOfPrimes(10_000);
+        expect(answer).toBe(5736396);
     })
 
 })
