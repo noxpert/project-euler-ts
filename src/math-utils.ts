0a88225d0390eebc61d0f35a743ce2ab0352c15f
Matthew Morgenegg <noxpert@gmail.com>
2024-11-16 11:15:07 -0700

    More performance improvements for isPrime

--- src/math-utils.ts
+++ src/math-utils.ts
@@ -13,22 +13,28 @@ export function isOdd(value: number): boolean {
 export function isPrime(value: number): boolean {
     if (value === 1) {
         return false;
-    } else if ([2, 3, 5, 7, 11].includes(value)) {
-        // Handle these first primes separately to allow the next checks.
+    } else if ([2, 3, 5, 7, 11, 13, 17, 19].includes(value)) {
+        // Handle these first primes separately before the more general checks below.
         return true;
-    } else if ([0, 2, 4, 5, 6, 8].includes(value % 10)) {
-        // A number ending with any of these is not prime,
-        // except 2 and 5 handled in the prior if.
+    } else if (isDivisible(value, 2)) {
         return false;
     } else if (isDivisible(value, 3)) {
+        return false;
+    } else if (isDivisible(value, 5)) {
         return false;
     } else if (isDivisible(value, 7)) {
         return false;
     } else if (isDivisible(value, 11)) {
         return false;
+    } else if (isDivisible(value, 13)) {
+        return false;
+    } else if (isDivisible(value, 17)) {
+        return false;
+    } else if (isDivisible(value, 19)) {
+        return false;
     }
-    for (let num: number = 13; num < value / 2; num += 2) {
-        // start iteration at 13, the first prime not handled above.
+    for (let num: number = 23; num <= Math.sqrt(value); num++) {
+        // start iteration at 23, the first prime not handled above.
         if (isDivisible(value, num)) {
             return false;
         }
